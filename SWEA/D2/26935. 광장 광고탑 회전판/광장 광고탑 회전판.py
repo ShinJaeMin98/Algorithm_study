a93fd122288cc653493f51d766888f5c7a7e1a92
ShinJaeMin98 <124487601+ShinJaeMin98@users.noreply.github.com>
2026-08-06 08:54:54 +0900

    [D2] Title: 광장 광고탑 회전판, Time: 78 ms, Memory: 53,888 KB -BaekjoonHub

--- SWEA/D2/26935. 광장 광고탑 회전판/광장 광고탑 회전판.py
+++ SWEA/D2/26935. 광장 광고탑 회전판/광장 광고탑 회전판.py
@@ -1,6 +1,20 @@
 # 단순 list 풀이
+# T = int(input())
+# for tc in range(1, T+1):
+#    N, M = map(int, input().split())
+#    nums = list(input().split())
+#    print(f'#{tc} {nums[M % N]}')    
+    
+# deque 풀이
+from collections import deque
+
 T = int(input())
 for tc in range(1, T+1):
     N, M = map(int, input().split())
-    nums = list(input().split())
-    print(f'#{tc} {nums[M % N]}')
+    queue = deque(list(input().split()))
+
+    for _ in range(M % N):
+        n = queue.popleft()
+        queue.append(n)
+
+    print(f'#{tc} {queue.popleft()}')
